905994833e115f3ef534b729fb80b211245777ba
Campbell He <duskmoon314@users.noreply.github.com>
2020-10-22 21:06:13 +0800

    💚 Fix CI unable to use hasura-cli (#4)
    * :green_heart: Fix CI unable to use hasura-cli

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,6 @@
 echo "$HASURA_GRAPHQL_ADMIN_SECRET"
 echo "$HASURA_GRAPHQL_JWT_SECRET"
+ls
+yarn add hasura-cli # install hasura-cli if it has been cleaned
 yarn hasura migrate apply --endpoint $HASURA_GRAPHQL_ENDPOINT
 yarn hasura metadata apply --endpoint $HASURA_GRAPHQL_ENDPOINT
