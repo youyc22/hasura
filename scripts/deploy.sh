2d1cffcd98d9c742370777ddeadbb20b493e0455
Campbell <hkp18@mails.tsinghua.edu.cn>
2020-12-30 00:53:19 +0800

    ci(actions): Migrate to Github Actions

--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-echo "$HASURA_GRAPHQL_ADMIN_SECRET"
-echo "$HASURA_GRAPHQL_JWT_SECRET"
-yarn add hasura-cli # install hasura-cli if it has been cleaned
-yarn hasura migrate apply --endpoint $HASURA_GRAPHQL_ENDPOINT
-yarn hasura metadata apply --endpoint $HASURA_GRAPHQL_ENDPOINT
