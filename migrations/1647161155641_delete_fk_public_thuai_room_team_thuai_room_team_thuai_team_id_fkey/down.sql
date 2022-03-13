c0ae71a4b0e39a201ffeef50d64907e95ed85f55
bryantsuen <bryantsuen@gmail.com>
2022-03-13 22:01:07 +0800

    fix: :fire: remove conflict files

--- migrations/1647161155641_delete_fk_public_thuai_room_team_thuai_room_team_thuai_team_id_fkey/down.sql
+++
@@ -1,1 +0,0 @@
-alter table "public"."thuai_room_team" add foreign key ("thuai_team_id") references "public"."thuai"("team_id") on update restrict on delete restrict;
--- migrations/1647161155641_delete_fk_public_thuai_room_team_thuai_room_team_thuai_team_id_fkey/up.sql
+++
@@ -1,1 +0,0 @@
-alter table "public"."thuai_room_team" drop constraint "thuai_room_team_thuai_team_id_fkey";
