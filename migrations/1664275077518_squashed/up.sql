30d646ba225fe71ddeb0faf718734246df82469a
L0510410 <1847254187@qq.com>
2022-09-30 16:09:55 +0800

    fix migration

--- migrations/1664275077518_squashed/up.sql
+++ migrations/1664275077518_squashed/up.sql
@@ -1,25 +1,4 @@
 
-
-DROP TABLE "public"."thuai_room_team";
-
-DROP TABLE "public"."thuai_room";
-
-DROP TABLE "public"."thuai_manager";
-
-DROP TABLE "public"."thuai_code";
-
-DROP TABLE "public"."team_member";
-
-DROP TABLE "public"."thuai";
-
-CREATE TABLE "public"."weekly"("id" int4 NOT NULL, "url" text NOT NULL, PRIMARY KEY ("id") , UNIQUE ("id"));
-
-ALTER TABLE "public"."weekly" ADD COLUMN "title" text NOT NULL;
-
-alter table "public"."weekly" add column "tags" json
- null;
-
-
 CREATE TABLE "public"."honors" ("name" text NOT NULL, PRIMARY KEY ("name") , UNIQUE ("name"));
 
 CREATE TABLE "public"."scholarships" ("code" text NOT NULL, "name" text NOT NULL, "amount" Integer NOT NULL, "type" text NOT NULL, "salutation" text, PRIMARY KEY ("code") , UNIQUE ("code"));
