bd028ca73368adbba95e8917c1adfd4ad3bbbfc4
xiangmy21 <2248278431@qq.com>
2023-12-17 23:17:17 +0800

    fix(aid_application): remove student_uuid migration

--- migrations/1702824878974_alter_table_public_aid_application_drop_column_student_uuid/down.sql
+++
@@ -1,2 +0,0 @@
-alter table "public"."aid_application" alter column "student_uuid" drop not null;
-alter table "public"."aid_application" add column "student_uuid" uuid;
--- migrations/1702824878974_alter_table_public_aid_application_drop_column_student_uuid/up.sql
+++
@@ -1,1 +0,0 @@
-alter table "public"."aid_application" drop column "student_uuid" cascade;
