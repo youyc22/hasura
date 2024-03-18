5f5cf2e88cd3618605625fe334fd05c514203b88
FranGuam <frankguansy@icloud.com>
2024-03-18 16:37:37 +0800

    feat: switches for contest

---
+++ migrations/1710750817344_alter_table_public_contest_add_column_arena_switch/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."contest" add column "arena_switch" boolean
+--  not null default 'False';
---
+++ migrations/1710751030097_alter_table_public_contest_add_column_playback_switch/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."contest" add column "playback_switch" boolean
+ not null default 'False';
