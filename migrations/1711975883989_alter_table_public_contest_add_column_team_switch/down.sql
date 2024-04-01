e3e4d06e464c42bb78583f6955a327e7aba06fc1
FranGuam <frankguansy@icloud.com>
2024-04-01 20:53:29 +0800

    feat(team): add `team_switch` field in `contest`

---
+++ migrations/1711975883989_alter_table_public_contest_add_column_team_switch/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."contest" add column "team_switch" boolean
+--  not null default 'false';
---
+++ migrations/1711975883989_alter_table_public_contest_add_column_team_switch/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."contest" add column "team_switch" boolean
+ not null default 'false';
