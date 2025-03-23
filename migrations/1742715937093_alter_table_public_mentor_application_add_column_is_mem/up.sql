c81766514d941df868a97b6f08dd14e9ce44063d
zzdhyb <139191187+zzdhybthu@users.noreply.github.com>
2025-03-23 15:47:43 +0800

    feat: mentor related (#198)

---
+++ migrations/1742715937093_alter_table_public_mentor_application_add_column_is_mem/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."mentor_application" add column "is_mem" boolean
+ not null default 'false';
---
+++ migrations/1742715979270_alter_table_public_mentor_application_alter_column_is_mem/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."mentor_application" rename column "is_member" to "is_mem";
